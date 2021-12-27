82f91a111e1782adb28942913a673da016f072ae
yogeshl <yogeshlcse@hotmail.com>
2021-12-26 21:17:46 -0500

    Working Samples

---
+++ counter/src/App.js
@@ -0,0 +1,23 @@
+import React from "react"
+
+export default function App() {
+    const [count, setCount] = React.useState(0)
+    
+    function add() {
+        setCount(count + 1)
+    }
+    
+    function subtract() {
+        setCount(count - 1)
+    }
+    
+    return (
+        <div className="counter">
+            <button className="counter--minus" onClick={subtract}>–</button>
+            <div className="counter--count">
+                <h1>{count}</h1>
+            </div>
+            <button className="counter--plus" onClick={add}>+</button>
+        </div>
+    )
+}
